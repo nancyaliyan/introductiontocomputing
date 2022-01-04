8c589ceeaa56708f700d82e38aa45ee5fc696662
nancyaliyan <50418951+nancyaliyan@users.noreply.github.com>
2022-01-04 13:15:59 +0530

    Delete asgdemo.py

--- asgdemo.py
+++
@@ -1,7 +0,0 @@
-Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a=12
->>> b=23
->>> print(a+b)
-35
->>> 
