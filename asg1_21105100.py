bc714325fd9a297b6103a889c33dc21154db8b18
nancyaliyan <50418951+nancyaliyan@users.noreply.github.com>
2022-01-06 16:52:36 +0530

    Add files via upload

---
+++ asg1_21105100.py
@@ -0,0 +1,21 @@
+#Assignment 1
+
+
+                 #1st program
+print("                   1st program")
+numb1=int(input("enter First number: "))
+numb2=int(input("enter second number: "))
+numb3=int(input("enter third number: "))
+avg=(numb1+numb2+numb3)/3
+print("avg is:",avg)
+
+#2nd program
+print("Student= [SID, Name, Gender, Dept. Name, CGPA]")
+student=[11,'nancy','F','CSE',7.8]
+print("student= ",student)
+#3rd program
+print("Marks of five students")
+Marks= [89,53,18,45,56]
+print("List before sorting",Marks)
+Marks.sort()
+print("List after Sorting",Marks)
