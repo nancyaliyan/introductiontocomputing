abddbd8c1ba9a0f6af27a4e389ff3cd6febf54a9
nancyaliyan <50418951+nancyaliyan@users.noreply.github.com>
2022-01-06 19:55:09 +0530

    Add files via upload

---
+++ qu1.py
@@ -0,0 +1,10 @@
+#Assignment 1
+
+
+                 #1st program
+print("                   1st program")
+numb1=int(input("enter First number: "))
+numb2=int(input("enter second number: "))
+numb3=int(input("enter third number: "))
+avg=(numb1+numb2+numb3)/3
+print("avg is:",avg)
